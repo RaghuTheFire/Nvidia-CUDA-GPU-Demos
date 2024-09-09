1fe85617a8b64e1b892cef6042ed46d2afead41b
Raghunath Nagaraj <67452178+RaghuTheFire@users.noreply.github.com>
2024-09-09 12:34:46 +0530

    Create CputoGpuBandWidth.cpp

---
+++ CputoGpuBandWidth.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cuda_runtime.h>
+
+void checkBandwidth() {
+    int deviceCount;
+    cudaGetDeviceCount(&deviceCount);
+    
+    for (int device = 0; device < deviceCount; ++device) {
+        cudaDeviceProp prop;
+        cudaGetDeviceProperties(&prop, device);
+        
+        std::cout << "Device " << device << ": " << prop.name << std::endl;
+        std::cout << "Memory Bandwidth (GB/s): " << prop.memoryClockRate * prop.memoryBusWidth / 8.0e6 << std::endl;
+    }
+}
+
+int main() {
+    checkBandwidth();
+    return 0;
+}
+
+
